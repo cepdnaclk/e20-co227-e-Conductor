53b3de394b5d98f1f20213f7b465a2c10dbc6f91
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2024-05-21 11:58:50 +0530

    Create: Sign in, Sign  up, Home
    Start to build those pages

--- 04. Web App/Web App/ReactJS/webapp-v01/src/App.js
+++ 04. Web App/Web App/ReactJS/webapp-v01/src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// This is the main code for the web app
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-        >
-          We are ready to build react app
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element={<Home/>}></Route>
+          <Route path = "/signin" element={<Signin/>}></Route>
+          <Route path = "/signup" element={<Signup/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
