eb6f3ae2bbbb466231d28cf54fdade1423dcc002
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2024-07-31 15:36:26 +0530

    Update Home.jsx

--- 04. Web App/Web App/ReactJS/webapp-v01/src/Pages/Home.jsx
+++ 04. Web App/Web App/ReactJS/webapp-v01/src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './Home.css';
 import useLiveLocation from '../Components/SessionData/useLiveLocation'
+import useCurrentGPSLocation from '../Components/SessionData/useCurrentGPSLocation';
 
 export default function Home({ language }) {
-  const liveData = useLiveLocation();
+  const liveData = useCurrentGPSLocation();
 
   return (
     <div>
@@ -12,13 +13,13 @@ export default function Home({ language }) {
 
       {/* <h2>Request Count: {requestCount}</h2> */}
 
-      <h4>Count: {liveData.coordinates.reqCount}</h4>
-      <h4>Lat: {liveData.coordinates.latitude}</h4>
-      <h4>Lng: {liveData.coordinates.longitude}</h4>
-      <h4>Acc: {liveData.coordinates.accuracy}</h4>
+      {/* <h4>Count: {liveData.coordinates.reqCount}</h4> */}
+      <h4>Lat: {liveData.coordinates.lat}</h4>
+      <h4>Lng: {liveData.coordinates.lng}</h4>
+      {/* <h4>Acc: {liveData.coordinates.accuracy}</h4>
       <h4>Alt: {liveData.coordinates.altitude}</h4>
       <h4>Spd: {liveData.coordinates.speed * 3.6} km/h</h4>
-      <h4>Hdg: {liveData.coordinates.heading}</h4>
+      <h4>Hdg: {liveData.coordinates.heading}</h4> */}
     </div>
   );
 }
