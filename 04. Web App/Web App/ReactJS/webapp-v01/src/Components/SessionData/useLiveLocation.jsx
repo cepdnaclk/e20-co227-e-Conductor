8e9aa8e16394f97e9e73f88636bf064e451e7e45
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2024-07-31 14:53:54 +0530

    New hook
    use to find the live location

--- 04. Web App/Web App/ReactJS/webapp-v01/src/Components/SessionData/useLiveLocation.jsx
+++ 04. Web App/Web App/ReactJS/webapp-v01/src/Components/SessionData/useLiveLocation.jsx
@@ -1,7 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function useLiveLocation() {
-    
-  return null;
+  const [liveData, setLiveData] = useState({
+    loaded: false,
+    coordinates:{
+      latitude: 0,
+      longitude: 0,
+      altitude: 0,
+      heading: 0,
+      speed: 0,
+      accuracy: 0,
+      reqCount:0
+    }    
+  }); 
+
+  // Checking permission for geolocation
+  useEffect(()=>{
+    // When an error occurs
+    const onError = error => {
+      setLiveData(prevData => ({
+        ...prevData,
+        loaded: true,
+        error,
+      }));
+    }  
+  
+    // When event is success
+    const onSuccess = position => {
+      const { accuracy, latitude, longitude, altitude, heading, speed } = position.coords;
+      
+      setLiveData(prev => ({
+        loaded: true,
+        coordinates: { latitude, longitude, accuracy, heading, speed, altitude, reqCount: prev.coordinates.reqCount + 1 },
+      }));
+    };
+
+    // Options
+    const options = {
+      enableHighAccuracy: false,
+      timeout: 5000,
+      maximumAge: 0
+    }
+
+    if(!("geoLocation" in navigator)){
+      onError({
+        code: 0,
+        message: "Geolocation is not supported",
+      });
+    }    
+
+    const watchId = navigator.geolocation.watchPosition(onSuccess, onError, options);
+
+    // Cleanup function to clear the watcher when the component unmounts
+    return () => navigator.geolocation.clearWatch(watchId);
+  },[]);
+
+  return liveData;
 }
