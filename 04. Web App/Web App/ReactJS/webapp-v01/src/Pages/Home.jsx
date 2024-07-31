8a3ed7de81245629377ce9c7d72a6946fe07a7a1
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2024-07-31 14:54:00 +0530

    Update Home.jsx

--- 04. Web App/Web App/ReactJS/webapp-v01/src/Pages/Home.jsx
+++ 04. Web App/Web App/ReactJS/webapp-v01/src/Pages/Home.jsx
@@ -1,57 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import './Home.css';
+import useLiveLocation from '../Components/SessionData/useLiveLocation'
 
 export default function Home({ language }) {
-  const [liveData, setLiveData] = useState({
-    latitude: 0,
-    longitude: 0,
-    altitude: 0,
-    heading: 0,
-    speed: 0,
-    accuracy: 0
-  });
-
-  const [requestCount, setRequestCount] = useState(0);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if (!navigator.geolocation) {
-      console.error('Geolocation is not supported by your browser');
-      return;
-    }
-
-    const successCallback = (position) => {
-      const { accuracy, latitude, longitude, altitude, heading, speed } = position.coords;
-      setLiveData({ accuracy, latitude, longitude, altitude, heading, speed });
-      setRequestCount(prevCount => prevCount + 1);
-      setError("Success");
-    };
-
-    const errorCallback = (error) => {
-      console.error('Error obtaining location', error);
-      setError('Error');
-    };
-
-    const watchId = navigator.geolocation.watchPosition(successCallback, errorCallback);
-
-    // Cleanup function to clear the watcher when the component unmounts
-    return () => navigator.geolocation.clearWatch(watchId);
-  }, []);
+  const liveData = useLiveLocation();
 
   return (
     <div>
       <h1>HOME</h1>
       <h3>Language: {language}</h3>
 
-      <h2>Request Count: {requestCount}</h2>
+      {/* <h2>Request Count: {requestCount}</h2> */}
 
-      <h4>Lat: {liveData.latitude}</h4>
-      <h4>Lng: {liveData.longitude}</h4>
-      <h4>Acc: {liveData.accuracy}</h4>
-      <h4>Alt: {liveData.altitude}</h4>
-      <h4>Spd: {liveData.speed}</h4>
-      <h4>Hdg: {liveData.heading}</h4>
-      {error && <h5>{error}</h5>}
+      <h4>Count: {liveData.coordinates.reqCount}</h4>
+      <h4>Lat: {liveData.coordinates.latitude}</h4>
+      <h4>Lng: {liveData.coordinates.longitude}</h4>
+      <h4>Acc: {liveData.coordinates.accuracy}</h4>
+      <h4>Alt: {liveData.coordinates.altitude}</h4>
+      <h4>Spd: {liveData.coordinates.speed * 3.6} km/h</h4>
+      <h4>Hdg: {liveData.coordinates.heading}</h4>
     </div>
   );
 }
