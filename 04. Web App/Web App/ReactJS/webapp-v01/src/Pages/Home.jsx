a76f1eb13907147f1c57029327ae4c4137e5779b
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2024-07-31 13:28:25 +0530

    Update Home.jsx

--- 04. Web App/Web App/ReactJS/webapp-v01/src/Pages/Home.jsx
+++ 04. Web App/Web App/ReactJS/webapp-v01/src/Pages/Home.jsx
@@ -1,45 +1,57 @@
-import React, { useState } from 'react'
-import './Home.css'
+import React, { useState, useEffect } from 'react';
+import './Home.css';
 
 export default function Home({ language }) {
-  var request = 0;
   const [liveData, setLiveData] = useState({
-    latitude:0,
-    longitude:0,
-    altitude:0,
-    heading:0,
-    speed:0,
-    accuracy:0
+    latitude: 0,
+    longitude: 0,
+    altitude: 0,
+    heading: 0,
+    speed: 0,
+    accuracy: 0
   });
 
-  navigator.geolocation.watchPosition(successCallback);
+  const [requestCount, setRequestCount] = useState(0);
+  const [error, setError] = useState(null);
 
-  function successCallback(position){
-    const { accuracy, latitude, longitude, altitude, heading, speed } = position.coords;
-    request = request + 1;
-    setLiveData({accuracy, speed, altitude, longitude, latitude, heading});
-  }
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      console.error('Geolocation is not supported by your browser');
+      return;
+    }
 
-  /* 
-  const watchID = navigator.geolocation.watchPosition(position => {
-    const { latitude, longitude } = position.coords;
-    console.log("watchID: " + watchID);
-  });
- */
+    const successCallback = (position) => {
+      const { accuracy, latitude, longitude, altitude, heading, speed } = position.coords;
+      setLiveData({ accuracy, latitude, longitude, altitude, heading, speed });
+      setRequestCount(prevCount => prevCount + 1);
+      setError("Success");
+    };
+
+    const errorCallback = (error) => {
+      console.error('Error obtaining location', error);
+      setError('Error');
+    };
+
+    const watchId = navigator.geolocation.watchPosition(successCallback, errorCallback);
+
+    // Cleanup function to clear the watcher when the component unmounts
+    return () => navigator.geolocation.clearWatch(watchId);
+  }, []);
+
   return (
-    <div >
+    <div>
       <h1>HOME</h1>
-      <h3>Language: {language} </h3>
+      <h3>Language: {language}</h3>
 
-      <h2>Request Count: {request}</h2>
-      
+      <h2>Request Count: {requestCount}</h2>
+
       <h4>Lat: {liveData.latitude}</h4>
       <h4>Lng: {liveData.longitude}</h4>
       <h4>Acc: {liveData.accuracy}</h4>
       <h4>Alt: {liveData.altitude}</h4>
       <h4>Spd: {liveData.speed}</h4>
       <h4>Hdg: {liveData.heading}</h4>
-
+      {error && <h5>{error}</h5>}
     </div>
-  )
+  );
 }
