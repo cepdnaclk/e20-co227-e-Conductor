361d43ec3f53c93c3ee641c091ee6f3621d21aeb
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2024-09-17 12:38:58 +0530

    Update Entrance.View.jsx

--- 04. Web App/Web App/ReactJS/webapp-v01/src/Components/Animations/Entrance.View.jsx
+++ 04. Web App/Web App/ReactJS/webapp-v01/src/Components/Animations/Entrance.View.jsx
@@ -1,54 +1,66 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 
-export function ViewFadeIn({ children, duration = 1000, delay = 0, sx }){
+export function ViewFadeIn({ children, duration = 1000, delay = 0, sx }) {
   return (
     <motion.div
       initial={{ opacity: 0 }}
-      transition={{ duration:duration/1000, delay:delay/1000 }}
+      transition={{ duration: duration / 1000, delay: delay / 1000 }}
       whileInView={{ opacity: 1 }}
       style={sx}
     >
       {children}
     </motion.div>
   );
-};
+}
 
-export function ViewFlyInX({ direction = 'left', duration = 1000, delay = 0, children, sx}){
+export function ViewFlyInX({
+  direction = "left",
+  duration = 1000,
+  delay = 0,
+  children,
+  sx,
+}) {
   return (
     <motion.div
-      initial={{ x: direction === 'left' ? -100 : 100, opacity: 0 }}
-      whileInView={{ x:0, opacity:1 }} 
-      transition={{ duration:duration/1000, delay:delay/1000 }}
+      initial={{ x: direction === "left" ? -100 : 100, opacity: 0 }}
+      whileInView={{ x: 0, opacity: 1 }}
+      transition={{ duration: duration / 1000, delay: delay / 1000 }}
       style={sx}
     >
       {children}
     </motion.div>
   );
-};
+}
 
-export function ViewFlyInY({ direction = 'top', duration = 1000, delay = 0, children, sx}){
+export function ViewFlyInY({
+  direction = "top",
+  duration = 1000,
+  delay = 0,
+  children,
+  sx,
+}) {
   return (
     <motion.div
-      initial={{ y: direction === 'top' ? -100 : 100, opacity: 0 }}
-      whileInView={{y:0, opacity:1}}
-      transition={{ duration:duration/1000, delay:delay/1000 }}
+      initial={{ y: direction === "top" ? -100 : 100, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ duration: duration / 1000, delay: delay / 1000 }}
       style={sx}
     >
       {children}
     </motion.div>
   );
-};
+}
 
-export function ViewZoomIn({ children, duration = 1000, delay = 0, sx }){
+export function ViewZoomIn({ children, duration = 1000, delay = 0, sx }) {
   return (
     <motion.div
       initial={{ scale: 0.5, opacity: 0 }}
       whileInView={{ scale: 1, opacity: 1 }}
-      transition={{ duration:duration/1000, delay:delay/1000 }}
+      transition={{ duration: duration / 1000, delay: delay / 1000 }}
       style={sx}
     >
       {children}
     </motion.div>
   );
-};
+}
