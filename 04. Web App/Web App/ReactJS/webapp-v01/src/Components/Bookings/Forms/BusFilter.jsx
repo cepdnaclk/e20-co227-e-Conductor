d804963ae20a7e1e1378204a4290dd9bbe2eb969
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2024-09-17 21:34:33 +0530

    Update BusFilter.jsx

--- 04. Web App/Web App/ReactJS/webapp-v01/src/Components/Bookings/Forms/BusFilter.jsx
+++ 04. Web App/Web App/ReactJS/webapp-v01/src/Components/Bookings/Forms/BusFilter.jsx
@@ -1,15 +1,31 @@
-import { Box, Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, Paper} from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import Stepper from '../../ProgressBars/Stepper';
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  Grid,
+  Paper,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Stepper from "../../ProgressBars/Stepper";
 
-
-export default function BusFilter({ steps, activeStep, handleBack,  setServiceFilter, setRouteFilter, setOrgFilter}) {
+export default function BusFilter({
+  steps,
+  activeStep,
+  handleBack,
+  setServiceFilter,
+  setRouteFilter,
+  setOrgFilter,
+}) {
   // variable to store service items states
   const [serviceItems, setServiceItems] = useState({
-    'super luxury': false,
-    'semi luxury': false,
-    'luxury': false,
-    'normal': false,
+    "super luxury": false,
+    "semi luxury": false,
+    luxury: false,
+    normal: false,
   });
 
   // variable to store route items states
@@ -26,129 +42,226 @@ export default function BusFilter({ steps, activeStep, handleBack,  setServiceFi
 
   // Handling service items
   const handleService = (event) => {
-    setServiceItems({...serviceItems, [event.target.value]: event.target.checked });
+    setServiceItems({
+      ...serviceItems,
+      [event.target.value]: event.target.checked,
+    });
   };
 
   // Handling route items
   const handleRoute = (event) => {
-    setRouteItems({...routeItems, [event.target.value]: event.target.checked });
+    setRouteItems({
+      ...routeItems,
+      [event.target.value]: event.target.checked,
+    });
   };
 
   // Handling organization catagory items
   const handleOrg = (event) => {
-    setOrgItems({...orgItems, [event.target.value]: event.target.checked });
+    setOrgItems({ ...orgItems, [event.target.value]: event.target.checked });
   };
 
   // Send service filter list
-  useEffect(()=>{
-    const checkedKeys = Object.keys(serviceItems).filter(key => serviceItems[key]);
+  useEffect(() => {
+    const checkedKeys = Object.keys(serviceItems).filter(
+      (key) => serviceItems[key]
+    );
     //console.log('Service Checked items:', checkedKeys);
     setServiceFilter(checkedKeys);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [serviceItems]);
 
   // Send route filter list
-  useEffect(()=>{
-    const checkedKeys = Object.keys(routeItems).filter(key => routeItems[key]);
+  useEffect(() => {
+    const checkedKeys = Object.keys(routeItems).filter(
+      (key) => routeItems[key]
+    );
     //console.log('Route Checked items:', checkedKeys);
     setRouteFilter(checkedKeys);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [routeItems]);
 
   // Send org filter list
-  useEffect(()=>{
-    const checkedKeys = Object.keys(orgItems).filter(key => orgItems[key]);
+  useEffect(() => {
+    const checkedKeys = Object.keys(orgItems).filter((key) => orgItems[key]);
     //console.log('Org Checked items:', checkedKeys);
     setOrgFilter(checkedKeys);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [orgItems]);
 
   return (
-    <Paper sx={{
-      width:'Calc(100% - 20px)', 
-      height:'70vh', 
-      minHeight:'400px', 
-      padding:'20px',  
-      display:'flex',
-      flexDirection:'column',
-      justifyContent: 'space-between',
-      margin:'10px 0'
-    }}>
-      <Stepper steps={steps} activeStep={activeStep} height={'100px'}/>
+    <Paper
+      sx={{
+        width: "Calc(100% - 20px)",
+        height: "70vh",
+        minHeight: "400px",
+        padding: "20px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        margin: "10px 0",
+      }}
+    >
+      <Stepper steps={steps} activeStep={activeStep} height={"100px"} />
 
-      <Box sx={{width:'100%', height:'calc(100% - 100px)', display:'flex', flexDirection:'column', justifyContent:'space-between'}}>
-        <Box  sx={{display:'flex', flexDirection:'column', gap:'20px'}}>
+      <Box
+        sx={{
+          width: "100%",
+          height: "calc(100% - 100px)",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box sx={{ display: "flex", flexDirection: "column", gap: "20px" }}>
           <FormControl component="fieldset">
-            <FormLabel component="legend" sx={{whiteSpace:'nowrap', fontFamily:'Open Sans', fontWeight:'bold'}} >Service Type</FormLabel>
-            <FormGroup aria-label="position" row sx={{display:'flex' , justifyContent:'space-between', width:'100%'}}>
+            <FormLabel
+              component="legend"
+              sx={{
+                whiteSpace: "nowrap",
+                fontFamily: "Open Sans",
+                fontWeight: "bold",
+              }}
+            >
+              Service Type
+            </FormLabel>
+            <FormGroup
+              aria-label="position"
+              row
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "100%",
+              }}
+            >
               <FormControlLabel
                 value="super luxury"
-                control={<Checkbox checked={serviceItems.super} onChange={handleService} />}
+                control={
+                  <Checkbox
+                    checked={serviceItems.super}
+                    onChange={handleService}
+                  />
+                }
                 label="Super Luxury"
                 labelPlacement="end"
               />
               <FormControlLabel
                 value="luxury"
-                control={<Checkbox checked={serviceItems.luxury} onChange={handleService} />}
+                control={
+                  <Checkbox
+                    checked={serviceItems.luxury}
+                    onChange={handleService}
+                  />
+                }
                 label="Luxury"
                 labelPlacement="end"
               />
               <FormControlLabel
                 value="semi luxury"
-                control={<Checkbox checked={serviceItems.semi} onChange={handleService} />}
+                control={
+                  <Checkbox
+                    checked={serviceItems.semi}
+                    onChange={handleService}
+                  />
+                }
                 label="Semi Luxury"
                 labelPlacement="end"
               />
               <FormControlLabel
                 value="normal"
-                control={<Checkbox checked={serviceItems.normal} onChange={handleService} />}
+                control={
+                  <Checkbox
+                    checked={serviceItems.normal}
+                    onChange={handleService}
+                  />
+                }
                 label="Normal"
                 labelPlacement="end"
               />
             </FormGroup>
           </FormControl>
 
-          <FormControl component="fieldset" fullWidth >
-            <FormLabel component="legend" sx={{whiteSpace:'nowrap', fontFamily:'Open Sans', fontWeight:'bold'}}>Route Type</FormLabel>
-            <FormGroup aria-label="position" row sx={{display:'flex' , width:'100%', }}>
-            <Grid container>
-              <Grid item xs={6}>
-                <FormControlLabel
-                  value="expressway"
-                  control={<Checkbox checked={routeItems.express} onChange={handleRoute} />}
-                  label="Expressway"
-                  labelPlacement="end"
-                />
+          <FormControl component="fieldset" fullWidth>
+            <FormLabel
+              component="legend"
+              sx={{
+                whiteSpace: "nowrap",
+                fontFamily: "Open Sans",
+                fontWeight: "bold",
+              }}
+            >
+              Route Type
+            </FormLabel>
+            <FormGroup
+              aria-label="position"
+              row
+              sx={{ display: "flex", width: "100%" }}
+            >
+              <Grid container>
+                <Grid item xs={6}>
+                  <FormControlLabel
+                    value="expressway"
+                    control={
+                      <Checkbox
+                        checked={routeItems.express}
+                        onChange={handleRoute}
+                      />
+                    }
+                    label="Expressway"
+                    labelPlacement="end"
+                  />
                 </Grid>
                 <Grid item xs={6}>
-                <FormControlLabel
-                  value="normalway"
-                  control={<Checkbox checked={routeItems.normal} onChange={handleRoute} />}
-                  label="Normalway"
-                  labelPlacement="end"
-                />
+                  <FormControlLabel
+                    value="normalway"
+                    control={
+                      <Checkbox
+                        checked={routeItems.normal}
+                        onChange={handleRoute}
+                      />
+                    }
+                    label="Normalway"
+                    labelPlacement="end"
+                  />
                 </Grid>
               </Grid>
             </FormGroup>
           </FormControl>
 
-          <FormControl component="fieldset" fullWidth >
-            <FormLabel component="legend" sx={{whiteSpace:'nowrap', fontFamily:'Open Sans', fontWeight:'bold'}}>Organization Catagory</FormLabel>
-            <FormGroup aria-label="position" row sx={{display:'flex' , width:'100%'}}>
+          <FormControl component="fieldset" fullWidth>
+            <FormLabel
+              component="legend"
+              sx={{
+                whiteSpace: "nowrap",
+                fontFamily: "Open Sans",
+                fontWeight: "bold",
+              }}
+            >
+              Organization Catagory
+            </FormLabel>
+            <FormGroup
+              aria-label="position"
+              row
+              sx={{ display: "flex", width: "100%" }}
+            >
               <Grid container>
                 <Grid item xs={6}>
                   <FormControlLabel
                     value="sltb"
-                    control={<Checkbox checked={orgItems.sltb} onChange={handleOrg} />}
+                    control={
+                      <Checkbox checked={orgItems.sltb} onChange={handleOrg} />
+                    }
                     label="SLTB"
                     labelPlacement="end"
                   />
                 </Grid>
                 <Grid item xs={6}>
                   <FormControlLabel
-                    value="privert"
-                    control={<Checkbox checked={orgItems.pvt} onChange={handleOrg} />}
-                    label="Privert"
+                    value="privart"
+                    control={
+                      <Checkbox checked={orgItems.pvt} onChange={handleOrg} />
+                    }
+                    label="Privart"
                     labelPlacement="end"
                   />
                 </Grid>
@@ -158,7 +271,9 @@ export default function BusFilter({ steps, activeStep, handleBack,  setServiceFi
         </Box>
 
         <Box>
-          <Button variant='outlined' fullWidth onClick={handleBack}>Back</Button>
+          <Button variant="outlined" fullWidth onClick={handleBack}>
+            Back
+          </Button>
         </Box>
       </Box>
     </Paper>
